ea5c8f32d32894d235542425b5e952e2f53d12b8
BridgetAlmas <BridgetAlmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2008-12-02 21:41:12 +0000

    fixing typo in mac build file

--- build_mac.sh
+++ build_mac.sh
@@ -2,8 +2,8 @@
 
 # specifing arch flag here rather than in CCFLAGS because CCFLAGS not set
 # consistently in makefiles
-export CC="gcc -arch ippc -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4"
-export LDFLAGS="-arch ippc -Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4"
+export CC="gcc -arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4"
+export LDFLAGS="-arch ppc -Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4"
 
 cd src
 make clean
